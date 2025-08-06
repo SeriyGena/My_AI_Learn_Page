33fd222196690dbdd9d1ac87f5b8b0c59953435b
SeriyGena <146438654+SeriyGena@users.noreply.github.com>
2025-08-06 18:42:56 +0300

    Create resources_data.js

---
+++ content/resources_data.js
@@ -0,0 +1,8 @@
+// Exported data for Resources section
+export const resourcesData = [
+    { title: "Master Machine Learning in Python with Scikit-Learn", author: "TM Quest", category: "ML" },
+    { title: "Time Series Analysis and Forecasting with Python", author: "Udemy", category: "Forecasting" },
+    { title: "Learn LangChain and LangGraph by building real world AI Agents", author: "Eden Marco", category: "Agents" },
+    { title: "Mastering OpenAI Python APIs: Unleash ChatGPT and GPT4", author: "Colt Steele", category: "API" },
+    { title: "Includes API, Web Services using REST, JSON...", author: "Nate Ross", category: "API" }
+];
